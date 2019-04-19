780e932eb5ccaae12ffebb131cbd63627940d6fa
Toni Lamont <tlam212@gmail.com>
2019-04-19 13:13:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badge_msg_array = []
+    array.each do |name|
+      badge_msg_array << "Hello, my name is #{name}."
+    end
+    badge_msg_array
+end
+
+def assign_rooms(array)
+    str_array = []
+    array.each_with_index do |name, i|
+     str_array << "Hello, #{name}! You'll be assigned to room #{i + 1}!"
+    end
+    str_array
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms(array)
+end
